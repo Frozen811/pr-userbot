c7405595c6751f17b274c79699c31c96e06de38e
Frozen811 <arikbozik@gmail.com>
2026-03-27 01:13:43 +0200

    Полная зачистка репозитория от мусора

--- logs.ps1
+++
@@ -1,7 +0,0 @@
-# --- НАСТРОЙКИ ---
-$ServerIP = "46.101.251.219"
-# -----------------
-
-Write-Host ">>> Connecting to Remote Logs..." -ForegroundColor Cyan
-Write-Host ">>> Press CTRL+C to exit log viewer (Bot stays online)." -ForegroundColor Yellow
-ssh -t root@$ServerIP "docker logs -f --tail 100 telegram_userbot"
